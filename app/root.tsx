59da84a6d4016a8ec7acfd25878606e388f27c7a
Aurélien Hervé <aurelien@bobsled.co>
2023-03-27 10:51:25 +0200

    title

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ import {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Le Cahier",
   viewport: "width=device-width,initial-scale=1",
 });
 
