b29aa82c81a52bb6434a2be1bee73b4a2e3d893f
Aurélien Hervé <aurelien@bobsled.co>
2023-04-25 21:20:56 +0200

    footer: add email

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,11 +1,14 @@
 import { Text, Link, Wrap } from '@chakra-ui/react';
 import { BsGithub } from 'react-icons/bs';
+import { MdOutlineEmail } from 'react-icons/md';
 
 export function Footer() {
   return (
     <Wrap align="center">
-      <Text>&copy; {new Date().getFullYear()}</Text>
-      <Link href="mailto:mail@aurelien-herve.com">Aurélien Hervé</Link>
+      <Text>&copy; {new Date().getFullYear()} Aurélien Hervé</Text>
+      <Link href="mailto:mail@aurelien-herve.com">
+        <MdOutlineEmail />
+      </Link>
       <Link href="https://github.com/aherve/le-cahier" isExternal={true}>
         <BsGithub />
       </Link>
